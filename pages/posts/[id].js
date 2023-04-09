9621a182d402702207a2539a30b576b5765006ad
tokai235 <the.summer.of.spiritual@gmail.com>
2023-04-09 23:13:40 +0900

    htmlコンテンツを表示

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,6 +10,9 @@ export default function Post({ postData }) {
       <br />
       {postData.date}
       <br />
+      <br />
+
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -24,7 +27,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
 
   return {
     props: {
